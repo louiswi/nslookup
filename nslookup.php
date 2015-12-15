5bd7c31edd2454b1099be83afe0e2877548b82b6
louiswi <lu814478913@gmail.com>
2015-12-15 23:26:02 +0800

    nslookup

---
+++ nslookup.php
@@ -0,0 +1,10 @@
+<?php 
+     if (isSet($_GET["host"])) 
+     { 
+         $host = $_GET["host"]; 
+         $ip = gethostbyname($host); 
+         if ($ip != $host) die ($ip); 
+     } 
+     echo "0.0.0.0"; 
+ ?>  
+ 
